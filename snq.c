810af5485b2d33e78291e0c8e6e816612fff7291
cpmachado <cpmachado@protonmail.com>
2025-02-01 00:25:21 +0000

    Document code, and remove superfluous caller func
    Also add int64_t types

--- snq.c
+++ snq.c
@@ -11,24 +11,30 @@
 #define VERSION "unknown"
 #endif
 
-#define MAXSIDE 8
+#define MAXSIDE 8L
 
 extern int errno;
 extern char *optarg;
 
+/* usage: Prints usage */
 void usage(void);
 
+/* version: Prints version */
 void version(void);
 
-int queenattacks(int *way, int k);
+/* valid_position: returns if position is valid, which means that queens aren't
+ * atacking each other
+ */
+int valid_position(int64_t *way, int k);
 
-void snqt(FILE *f, int *way, int k, int n);
-
-void snq(FILE *f, int *way, int n);
+/* snq: Checks and prints solutions for puzzle given a 'n' side chess board,
+ * for given step k of combination
+ */
+void snq(FILE *f, int64_t *way, int k, int n);
 
 int main(int argc, char *argv[]) {
   FILE *f = NULL;
-  int way[MAXSIDE] = {0};
+  int64_t way[MAXSIDE] = {0};
   int64_t n = 0;
   int opt;
 
@@ -41,14 +47,14 @@ int main(int argc, char *argv[]) {
       version();
       exit(0);
     case 'n':
-      n = strtoll(optarg, NULL, 10);
+      n = strtol(optarg, NULL, 10);
       if (errno) {
         fprintf(stderr, "%s\n", strerror(errno));
         usage();
         exit(1);
       }
-      if (n < 1 || n > 8) {
-        fprintf(stderr, "Number should be in [1, 8]\n");
+      if (n < 1 || n > MAXSIDE) {
+        fprintf(stderr, "Number should be in [1, %ld]\n", MAXSIDE);
         exit(1);
       }
       break;
@@ -74,66 +80,64 @@ int main(int argc, char *argv[]) {
   }
 
   if (!n) {
-    n = 8;
+    n = MAXSIDE;
   }
 
-  snq(f, way, n);
+  snq(f, way, 0, n);
 
   fclose(f);
   return 0;
 }
 
 void usage(void) {
-  fprintf(stdout, "snq is a simple N-Queen Problem solver.\n");
-  fprintf(stdout, "Usage: snq [OPTIONS]\n\n");
+  fprintf(stdout, "snq is a simple N-Queen Problem solver.\n"
+                  "Usage: snq [OPTIONS]\n\n"
 
-  fprintf(stdout, "Options:\n");
-  fprintf(stdout, "    -h          -- display help and exit\n");
-  fprintf(stdout, "    -v          -- display version and exit\n");
-  fprintf(stdout, "    -n [NUMBER] -- Length of side(defaults to 8, should be "
-                  "in [1, 8])\n");
-  fprintf(stdout, "    -o [FILE]   -- output to file(defaults to stdout)\n");
+                  "Options:\n"
+                  "    -h          -- display help and exit\n"
+                  "    -v          -- display version and exit\n"
+                  "    -n [NUMBER] -- Length of side(defaults to 8, should be "
+                  "in [1, 8])\n"
+                  "    -o [FILE]   -- output to file(defaults to stdout)\n");
 }
 
 void version(void) {
   fprintf(stdout, "snq-" VERSION " Copyright (C) 2019 Carlos Pinto Machado\n");
 }
 
-int queenattacks(int *way, int k) {
+int valid_position(int64_t *way, int k) {
   int i;
+  int64_t d;
 
   for (i = 0; i < k; i++) {
-    if ((way[i] == way[k]) || (abs(way[i] - way[k]) == (k - i))) {
+    d = labs(way[i] - way[k]);
+    if ((way[i] == way[k]) || d == (k - i)) {
       return 0;
     }
   }
   return 1;
 }
 
-void snqt(FILE *f, int *way, int k, int n) {
+void print_solution(FILE *f, int64_t *way, int n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    fprintf(f, "%ld", way[i]);
+  }
+  fprintf(f, "\n");
+}
+
+void snq(FILE *f, int64_t *way, int k, int n) {
   int i;
 
   if (k == n) {
-    for (i = 0; i < n; i++) {
-      fprintf(f, "%d", way[i]);
-    }
-    fprintf(f, "\n");
+    print_solution(f, way, n);
     return;
   }
 
   for (i = 0; i < n; i++) {
     way[k] = i;
-    if (queenattacks(way, k)) {
-      snqt(f, way, k + 1, n);
+    if (valid_position(way, k)) {
+      snq(f, way, k + 1, n);
     }
-  }
-}
-
-void snq(FILE *f, int *way, int n) {
-  int i;
-
-  for (i = 0; i < n; i++) {
-    way[0] = i;
-    snqt(f, way, 1, n);
   }
 }
